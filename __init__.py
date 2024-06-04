f593f718d2e6ea972c0330f0babac1926b34d4a6
Carlos Acosta <ceacosta.jobs@gmail.com>
2024-06-03 23:22:36 -0700

    feat: adds CORS examples

--- __init__.py
+++ __init__.py
@@ -1,7 +1,19 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    # CORS(app)
+    # anyone can access resources under /api/
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    # CORS HEADERS
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
 
     @app.route("/")
     def hello():
@@ -10,5 +22,10 @@ def create_app(test_config=None):
     @app.route("/smiley")
     def smiley():
         return "=)"
+    
+    @app.route("/cors_messages")
+    @cross_origin()
+    def messages():
+        return "Getting CORS messages"
 
     return app
